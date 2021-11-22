ba545d72e896cc92253d2f693cf24d07ab92df47
mamdouhramadan <mamdouhalamassi@gmail.com>
2021-11-22 17:03:01 +0400

    Setup Files

---
+++ src/Screens/App.js
@@ -0,0 +1,13 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h3 className="text-center my-4">Hello World</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
